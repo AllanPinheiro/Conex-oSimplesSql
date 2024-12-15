45bc48ccac90f1829875d9a58881c3a4cf548cfb
pinheirogabrieldev@gmail.com <pinheirogabrieldev@gmail.com>
2024-12-15 16:20:49 -0300

    Adicionar arquivos de projeto.

---
+++ ConexãoSimplesSql/ConnectionDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConexãoSimplesSql
+{
+    internal class ConnectionDB
+    {
+        public ConnectionDB()
+        {
+            // String de conexão com o banco de dados
+            string connectionString = "Data Source=DESKTOP-N6OGC4V\\SQLEXPRESS;Initial Catalog=ClientesCadastrados;Integrated Security=True;Encrypt=True;Trust Server Certificate=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("Conexão aberta com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Erro ao abrir a conexão: " + ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("Conexão fechada com sucesso!");
+                }
+            }
+        }
+    }
+}
